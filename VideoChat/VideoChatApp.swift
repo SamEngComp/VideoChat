f14544d46210aad16eb9aa173635166588390de3
Samuel S. Furtado <salessamuel268@gmail.com>
2025-05-28 22:35:44 -0300

    Initial Commit

---
+++ VideoChat/VideoChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoChatApp.swift
+//  VideoChat
+//
+//  Created by Samuel Sales on 28/05/25.
+//
+
+import SwiftUI
+
+@main
+struct VideoChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
